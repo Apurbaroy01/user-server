5390044b2d067ac6325efb400e3a830c3dc77fa2
Apurba Roy <apurbaroy00011@gmail.com>
2025-07-04 00:14:12 +0600

    1st

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = "mongodb+srv://adrikaroy487:z1dZJHilxwwf00q9@cluster0.znyn8om.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("✅ Connected to MongoDB!");
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+
+    // Example route to get users
+
+    
+    // app.get('/users', async (req, res) => {
+    //   const users = await userCollection.find().toArray();
+    //   res.send(users);
+    // });
+
+    // Example route to add a user
+
+
+    // app.post('/users', async (req, res) => {
+    //   const newUser = req.body;
+    //   const result = await userCollection.insertOne(newUser);
+    //   res.send(result);
+    // });
+
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+run();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
